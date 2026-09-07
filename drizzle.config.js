810963609f33e63ad6048b823eec57b6b1e3b7f5
ankitkgupta11-g <ankitkgupta1123@gmail.com>
2026-09-08 01:25:24 +0530

    Initial commit

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.tsx",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: 'postgresql://neondb_owner:npg_qk0Y6zTharEB@ep-cold-frog-zagk1vkr-pooler.c-2.eu-west-2.aws.neon.tech/Creator%20Ai?sslmode=require&channel_binding=require',
+
+    }
+  };
+  
